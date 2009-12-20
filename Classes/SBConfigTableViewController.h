293ddca31821e2818346c214dcf7a59988196f97
sugyan <sugi1982@gmail.com>
2009-12-21 02:05:49 +0900

    Config画面基本枠組み

---
+++ Classes/SBConfigTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SBConfigTableViewController.h
+//  Sabotter
+//
+//  Created by sugyan on 09/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SBConfigTableViewController : UITableViewController {
+
+}
+
+@end
